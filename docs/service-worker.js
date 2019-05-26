d3be358dab11ef55eda607b560d1e1ec0f7df1d0
tomoko <t0m0k05238188+mac@gmail.com>
2019-05-26 16:14:16 +0900

    fix workbox version

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/bgsyncerrr/precache-manifest.823cdfc80856a67f62797ab0a73fc1d7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/bgsyncerrr/precache-manifest.823cdfc80856a67f62797ab0a73fc1d7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "bgsyncerr"});
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
